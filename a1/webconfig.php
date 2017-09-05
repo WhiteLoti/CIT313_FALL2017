6e7cdf7f9c4a7f42199b1f8722987caadfe59d9a
WhiteLoti <nathan_thomas007@yahoo.com>
2017-09-05 01:30:34 -0400

    Delete webconfig.php

--- a1/webconfig.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Remove Later for Security
-ini_set('display_errors',1);
-error_reporting(E_ALL);
-
-define('ABSOLUTE_PATH', "./");
-// Not really an error but changed URL to the server host and correct directory path
-define('URL_ROOT',"http://{$_SERVER['HTTP_HOST']}/CIT313/FA2017/a1");
