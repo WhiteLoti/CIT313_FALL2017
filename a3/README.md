71b07873bac2b99592c60031c8f8071f06971d65
WhiteLoti <nathan_thomas007@yahoo.com>
2017-10-19 00:18:22 -0400

    Add files via upload

---
+++ a3/README.md
@@ -0,0 +1,4 @@
+Assignment 3: Building A Framework
+==============
+
+Source files for Week 8
---
+++ a3/application/config.php
@@ -0,0 +1,9 @@
+<?php
+define('DEFAULT_VIEW', 'home');//set this to any page to be the default home page
+define('BASE_URL', 'http://corsair.cs.iupui.edu:22161/CIT313/FA2017/a3/');
+
+//database info
+define('DB_HOST', 'localhost');
+define('DB_USER', 'thomanat');
+define('DB_PASS', 'I love killer Wh@les');
+define('DB_NAME', 'thomanat_db');
